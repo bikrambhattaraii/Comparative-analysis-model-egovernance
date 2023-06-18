1df8eb93a713e30982d5f2ebc7b73d5c49f2c41f
bikram bhattarai <bikrambhattarai296@gmail.com>
2023-06-18 22:24:47 +0545

    updated

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,19 +1,22 @@
 import "./form.css";
 import { useState } from "react";
 const Form = () => {
+  
+
+
+
+
   const [formData, setFormData] = useState({
-    country_name: '',
-    start_date:'',
-    history:'',
-    epassport:'',
-    birthdeathcert:'',
-    water_payment:'',
-    electricity_payment:'',
-    mobile_services:'',
-    blockchain_adoption:'',
-    electronic_voting:'',
-    created_at:'',
-    updated_at:'',
+    country_name: "",
+    start_date: "",
+    history: "",
+    epassport: "",
+    birthdeathcert: "",
+    water_payment: "",
+    electricity_payment: "",
+    mobile_services: "",
+    blockchain_adoption: "",
+    electronic_voting: "",
   });
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -25,100 +28,128 @@ const Form = () => {
     console.log(formData);
     // Reset form data
     setFormData({
-      country_name: '',
-      start_date:'',
-      history:'',
-      epassport:'',
-      birthdeathcert:'',
-      water_payment:'',
-      electricity_payment:'',
-      mobile_services:'',
-      blockchain_adoption:'',
-      electronic_voting:'',
-      created_at:'',
-      updated_at:'',
+      country_name: "",
+      start_date: "",
+      history: "",
+      epassport: "",
+      birthdeathcert: "",
+      water_payment: "",
+      electricity_payment: "",
+      mobile_services: "",
+      blockchain_adoption: "",
+      electronic_voting: "",
     });
   };
-
   return (
     <div className="form-container">
       <h1>upload new country </h1>
       <form action="" method="POST" onSubmit={handleSubmit}>
         <div>
-          <label>Country Name:
-            <input type="text" 
-            placeholder="enter the country name" 
-            value={formData.country_name}
-            onChange={handleInputChange}/>
+          <label>
+            Country Name:
+            <input
+              type="text"
+              name="country_name"
+              placeholder="enter the country name"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
-          <label>E-governannce Start date:
-            <input type="date"
-            onChange={handleInputChange} 
-            value={formData.start_date}/>
+          <label>
+            E-governannce Start date:
+            <input
+              type="date"
+              name="start-date"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
-          <label>History:
-            <input type="text" placeholder="enter the history of e governance of the specific country"
-            onChange={handleInputChange} 
-            value={formData.history}/>
+          <label>
+            History:
+            <input
+              type="text"
+              name="history"
+              placeholder="enter the history of e governance of the specific country"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
-          <label>E passport:
-            <input type='checkbox'
-            value={formData.epassport}
-            onChange={handleInputChange} />
+          <label>
+            E passport:
+            <input
+            
+              type="checkbox"
+              name="epassport"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
-          <label>Birth Death Registration:
-           <input type="checkbox" 
-           value={formData.birthdeathcert}
-           onChange={handleInputChange}/> 
-          </label>  
-        </div>
-        <div>
-          <label>water-tap charges:
-           <input type="checkbox"
-           value={formData.water_payment}
-           onChange={handleInputChange}/> 
+          <label>
+            Birth Death Registration:
+            <input
+              type="checkbox"
+              name="birthdeathcert"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
-          <label>Eelectricity bill:
-           <input type="checkbox"
-           value={formData.electricity_payment}
-           onChange={handleInputChange}/> 
+          <label>
+            water-tap charges:
+            <input
+              type="checkbox"
+              name="water_payment"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
-          <label>mobile services:
-           <input type="checkbox" 
-           value={formData.mobile_services}
-           onChange={handleInputChange}/> 
-          </label>
-
-        </div>
-        <div>
-          <label>Blockchain Adoption:
-           <input type="checkbox"  
-           value={formData.blockchain_adoption}
-           onChange={handleInputChange}/> 
+          <label>
+            Eelectricity bill:
+            <input
+              type="checkbox"
+              name="electricity_payment"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <div>
-          <label>Eelectronic voting:
-           <input type="checkbox" 
-           value={formData.electronic_voting}
-           onChange={handleInputChange}/> 
+          <label>
+            mobile services:
+            <input
+              type="checkbox"
+              name="mobile_services"
+              
+              onChange={handleInputChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Blockchain Adoption:
+            <input
+              type="checkbox"
+              name="blockchain_adoption"
+              onChange={handleInputChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Eelectronic voting:
+            <input
+              type="checkbox"
+              name="electronic_voting"
+              onChange={handleInputChange}
+            />
           </label>
         </div>
         <input type="submit"  />
       </form>
-      
     </div>
   );
 };
--- src/components/homesection/homesection.jsx
+++ src/components/homesection/homesection.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import "./homesection.css"
 import Header from '../header/header'
 import Compare from './compare/compare'
+import { useNavigate } from 'react-router-dom';
 
 const Homesection = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/form'); // Navigate to '/component2'
+  };
   return (
-    <div>
+    <div className='main-container-homesection'>
       <Header />
-      <Compare />
+      <h1>Compare two different countries e governanace </h1>
+      <button className='btn2' onClick={handleClick}>proceed to form</button>
     </div>
   )
 }

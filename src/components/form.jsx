472392dbbcee06e03c9f5ce85fa97cb192f2415c
rikesh-a11 <rikeshshrestha499@gmail.com>
2023-06-19 12:02:37 +0545

    form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -42,83 +42,84 @@ const Form = () => {
 
   return (
     <div className="form-container">
-      <h1>upload new country </h1>
+      <h1>Upload Country Details</h1>
       <form action="" method="POST" onSubmit={handleSubmit}>
-        <div>
+        <div className="form-data">
           <label>Country Name:
-            <input type="text" 
-            placeholder="enter the country name" 
+          <input type="text" 
+            placeholder="Enter the Country Name" 
             value={formData.country_name}
             onChange={handleInputChange}/>
           </label>
         </div>
-        <div>
+        <div className="form-data">
           <label>E-governannce Start date:
             <input type="date"
             onChange={handleInputChange} 
             value={formData.start_date}/>
           </label>
         </div>
-        <div>
+        <div className="form-data">
           <label>History:
-            <input type="text" placeholder="enter the history of e governance of the specific country"
+            <input type="text" placeholder="Enter the history of E-governance "
             onChange={handleInputChange} 
             value={formData.history}/>
           </label>
         </div>
-        <div>
+        
+        <div className="form-field">
           <label>E passport:
             <input type='checkbox'
             value={formData.epassport}
             onChange={handleInputChange} />
           </label>
         </div>
-        <div>
+        <div className="form-field">
           <label>Birth Death Registration:
            <input type="checkbox" 
            value={formData.birthdeathcert}
            onChange={handleInputChange}/> 
           </label>  
         </div>
-        <div>
+        <div className="form-field">
           <label>water-tap charges:
            <input type="checkbox"
            value={formData.water_payment}
            onChange={handleInputChange}/> 
           </label>
         </div>
-        <div>
+        <div className="form-field">
           <label>Eelectricity bill:
            <input type="checkbox"
            value={formData.electricity_payment}
            onChange={handleInputChange}/> 
           </label>
         </div>
-        <div>
+        <div className="form-field">
           <label>mobile services:
            <input type="checkbox" 
            value={formData.mobile_services}
            onChange={handleInputChange}/> 
           </label>
-
         </div>
-        <div>
+        <div className="form-field">
           <label>Blockchain Adoption:
            <input type="checkbox"  
            value={formData.blockchain_adoption}
            onChange={handleInputChange}/> 
           </label>
         </div>
-        <div>
+        <div className="form-field">
           <label>Eelectronic voting:
            <input type="checkbox" 
            value={formData.electronic_voting}
            onChange={handleInputChange}/> 
           </label>
         </div>
+        <div>
         <input type="submit"  />
+        </div>
       </form>
-      
     </div>
   );
 };
